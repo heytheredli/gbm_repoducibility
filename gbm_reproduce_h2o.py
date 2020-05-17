d95532cd0506dd23d3e3f08c819627b4af6ec80a
Daniel Li <daniel.li317@gmail.com>
2020-05-17 17:25:01 +0000

    updates to h2o script

--- gbm_reproduce_h2o.py
+++ gbm_reproduce_h2o.py
@@ -11,6 +11,7 @@ from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split, GridSearchCV
 
 import multiprocessing
+import random
 
 # prep data
 diabetes = datasets.load_diabetes()
@@ -20,7 +21,8 @@ X = pd.DataFrame(X)
 
 for col in X.columns.tolist():
     for _ in range(10):
-        X[f'{col}_{_}'] = np.random.permutation(X[col].values)
+        #X[f'{col}_{_}'] = np.random.permutation(X[col].values)
+        X[f'{col}_{_}'] = random.random()
 
 X = np.array(X)
 
@@ -44,20 +46,16 @@ param = {
       "ntrees" : 100
     , "max_depth" : 10
     , "learn_rate" : 0.02
-    , "sample_rate" : 0.7
-    , "col_sample_rate_per_tree" : 0.9
     , "min_rows" : 5
     , "seed": 42
-    , "score_tree_interval": 100
 }
 from h2o.estimators import H2OXGBoostEstimator
 model = H2OXGBoostEstimator(**param)
 
-model.train(y = "response", training_frame = df_train)
+model.train(x = list(range(110)), y = "response", training_frame = df_train, validation_frame = df_test)
 prediction = model.predict(df_test)
 
 auc = roc_auc_score(y_test, prediction.as_data_frame())
 print(f'h2o auc: {auc}')
 print(model.params.get('seed').get('actual'))
-print(df_train.head())
 
